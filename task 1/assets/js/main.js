1519936fd4a006fa7c7a640ec32513bcd7a71384
kamil <127212995+dymolite@users.noreply.github.com>
2023-04-14 22:17:15 +0400

    loop-hometask

--- task 1/assets/js/main.js
+++ task 1/assets/js/main.js
@@ -1,23 +1,7 @@
-/* let num = prompt();
-let sum = 0;
-
-while () {
-  
-let a = num % 10;
-let b = ((num-a)/10) % 10;
-let c = (((num-a) / 10  -b) / 10) % 10;
-let d = (((num - a)/10 - b)/ 10 -c) / 10;
-
-let sum= a + b + c + d
-}
-
-prompt( sum);
- */
-
 var num = prompt(),
     sum = 0;
 
-while (num) {
+while (num>0) {
     sum += num % 10;
     num = Math.floor(num / 10);
 }
